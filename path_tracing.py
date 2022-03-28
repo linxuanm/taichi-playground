6162a1e44a87c5863bba30a9efb61ede2186349b
BuildTools <davidmaamoaix@gmail.com>
2022-03-28 23:31:20 +0800

    other spheres

--- path_tracing.py
+++ path_tracing.py
@@ -5,3 +5,57 @@ import ray_tracing_utils as rtu
 
 ti.init(debug=True)
 
+size = 512
+
+camera = rtu.Camera(
+    ti.Vector([0, 1, -5], dt=ti.f32),
+    ti.Vector([0, 0, 1], dt=ti.f32)
+)
+scene = rtu.Scene()
+
+WALL_MAT = (rtu.MAT_DIFFUSE, ti.Vector([0.8, 0.8, 0.8]))
+scene.add(rtu.Sphere(ti.Vector([0, -100.5, -1]), 100, WALL_MAT))
+scene.add(rtu.Sphere(ti.Vector([0, 102.5, -1]), 100, WALL_MAT))
+scene.add(rtu.Sphere(ti.Vector([0, 1.0, 101.0]), 100, WALL_MAT))
+scene.add(rtu.Sphere(
+    ti.Vector([-101.5, 0, -1]), 100, (rtu.MAT_DIFFUSE, ti.Vector([0.6, 0, 0])
+)))
+scene.add(rtu.Sphere(
+    ti.Vector([101.5, 0, -1]), 100, (rtu.MAT_DIFFUSE, ti.Vector([0.0, 0, 0.6])
+)))
+
+scene.add(rtu.Sphere(
+    ti.Vector([0.7, 0, -0.5]),
+    0.5, (rtu.MAT_DIELECTRIC, ti.Vector([1.0, 1.0, 1.0]))
+))
+scene.add(rtu.Sphere(
+    ti.Vector([-0.8, 0.2, -1]),
+    0.7, (rtu.MAT_METAL, ti.Vector([0.6, 0.8, 0.8]))
+))
+
+pixels = ti.Vector.field(3, ti.f32, shape=(size, size))
+
+
+@ti.kernel
+def render():
+    for i, j in pixels:
+        u = i / size
+        v = j / size
+        color = ti.Vector([0, 0, 0], dt=ti.f32)
+
+        ray = camera.get_camera_ray(u, v)
+        hit, hit_pos, hit_normal, mat, hit_color = scene.get_ray_hit(ray)
+
+        if hit:
+            color = hit_color
+
+        pixels[i, j] = color
+
+
+gui = ti.GUI('Path Tracing Demo', res=(size, size))
+
+while gui.running:
+    render()
+
+    gui.set_image(pixels)
+    gui.show()
--- ray_tracing_utils.py
+++ ray_tracing_utils.py
@@ -1,7 +1,17 @@
 import taichi as ti
 
 
-UP = ti.Vector([0, 1, 0])
+MAT_LIGHT = 0
+MAT_DIFFUSE = 1
+MAT_DIELECTRIC = 2
+MAT_METAL = 3
+
+UP = ti.Vector([0, 1, 0], dt=ti.f32)
+
+
+@ti.func
+def zero():
+    return ti.Vector([0, 0, 0], dt=ti.f32)
 
 
 @ti.data_oriented
@@ -9,7 +19,7 @@ class Ray:
 
     def __init__(self, origin, dir, t_min=1e-3, t_max=1e+8):
         self.origin = origin
-        self.direction = direction
+        self.direction = dir
         self.t_min = t_min
         self.t_max = t_max
 
@@ -23,7 +33,8 @@ class SceneObject:
 
     def __init__(self, pos, mat):
         self.pos = pos
-        self.mat = mat
+        self.mat = mat[0]
+        self.color = mat[1]
 
     @ti.func
     def hit_ray(self, ray):
@@ -34,22 +45,23 @@ class SceneObject:
 class Sphere(SceneObject):
 
     def __init__(self, pos, radius, mat):
-        super(pos, mat)
+        super().__init__(pos, mat)
         self.radius = radius
 
     @ti.func
     def hit_ray(self, ray):
         to_origin = ray.origin - self.pos
 
-        a = 1 #ray.direction.dot(ray.direction)
+        a = ray.direction.dot(ray.direction)
         b = 2 * ray.direction.dot(to_origin)
-        c = to_origin.dot(to_origin) - self.radius ** 2
+        c = to_origin.dot(to_origin) - self.radius * self.radius
+
         discrim = b * b - 4 * a * c
         root = 0
 
         hit = False
-        hit_pos = ti.Vector([0, 0, 0])
-        hit_normal = ti.Vector([0, 0, 0])
+        hit_pos = zero()
+        hit_normal = zero()
 
         if discrim > 0: # ignore = 0 cuz artifacts
             offset = ti.sqrt(discrim)
@@ -65,12 +77,12 @@ class Sphere(SceneObject):
 
         if hit:
             hit_pos = ray.at(root)
-            hit_normal = (hit_pos - self.center).normalize()
+            hit_normal = (hit_pos - self.pos).normalized()
 
             if hit_normal.dot(ray.direction) > 0: # view from inside of sphere
                 hit_normal = -hit_normal
 
-        return hit, hit_pos, hit_normal
+        return hit, hit_pos, hit_normal, self.mat, self.color
 
 
 @ti.data_oriented
@@ -86,24 +98,29 @@ class Scene:
     def get_ray_hit(self, ray):
         closest = ray.t_max
         hit = False
-        hit_pos = ti.Vector([0.0, 0.0, 0.0])
-        hit_normal = ti.Vector([0.0, 0.0, 0.0])
+        hit_pos = zero()
+        hit_normal = zero()
+        mat = MAT_DIFFUSE
+        color = zero()
 
-        for i in self.objs:
-            curr_hit, curr_hit_pos, curr_hit_normal = i.hit_ray(ray):
+        for i in ti.static(range(len(self.objs))):
+            curr_hit, curr_hit_pos, curr_hit_normal, curr_mat, curr_color = \
+                self.objs[i].hit_ray(ray)
 
             if curr_hit:
                 hit = curr_hit
                 hit_pos = curr_hit_pos
                 hit_normal = curr_hit_normal
+                mat = curr_mat
+                color = curr_color
 
-        return hit, hit_pos, hit_normal
+        return hit, hit_pos, hit_normal, mat, color
 
 
 @ti.data_oriented
 class Camera:
 
-    def __init__(pos, view_dir, fov=60.0, aspect_ratio=1.0):
+    def __init__(self, pos, view_dir, fov=60.0, aspect_ratio=1.0):
         self.fov = fov
         self.ratio = aspect_ratio
 
@@ -116,7 +133,6 @@ class Camera:
 
         self.initialize(pos, view_dir)
 
-    @ti.kernel
     def initialize(self, pos, view_dir):
         self.pos[None] = pos
         self.direction[None] = view_dir
@@ -125,11 +141,11 @@ class Camera:
         half_height = ti.tan(angle / 2)
         half_width = self.ratio * half_height
 
-        right = self.direction[None].cross(UP).normalize()
+        right = self.direction[None].cross(UP).normalized()
         up = right.cross(self.direction[None])
 
         offset = -half_height * up - half_width * right
-        self.lower_left[None] = self.pos[None] + offset
+        self.lower_left[None] = self.pos[None] + self.direction[None] + offset
 
         self.u_dir[None] = 2 * half_width * right
         self.v_dir[None] = 2 * half_width * up
@@ -137,5 +153,5 @@ class Camera:
     @ti.func
     def get_camera_ray(self, u, v):
         offset = u * self.u_dir[None] + v * self.v_dir[None]
-        direction = self.lower_left[None] + offset
-        return Ray(self.pos, direction)
+        direction = self.lower_left[None] + offset  - self.pos[None]
+        return Ray(self.pos[None], direction.normalized())
